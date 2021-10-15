d461f98413524e80e07791aa1fffa319febd1a69
MFLuce <mfluce@yahoo.fr>
2021-10-15 12:19:51 +0200

    Add nodemailer function

--- routes/intranet.js
+++ routes/intranet.js
@@ -18,6 +18,10 @@ const isAdmin = require("../middleware/isAdmin");
 const PostMiddleware = require("../middleware/PostMiddleware");
 const formatedDate = require("../utils/formatedDate");
 
+//NODEMAILER
+const nodemailer = require("nodemailer");
+
+//ADMIN DASHBOARD ROUTES
 router.get("/admin-dashboard", isLoggedIn, isAdmin, (req, res) => {
   //1) Query the DB and get us all the users in
   // which the property isCompany === true!
@@ -42,28 +46,210 @@ router.get("/admin-dashboard", isLoggedIn, isAdmin, (req, res) => {
     });
 });
 
+// ASSIGN POST ROUTES
 router.post("/:id/assign", isLoggedIn, isAdmin, PostMiddleware, (req, res) => {
   const { assignedTo } = req.body;
 
   return Post.findByIdAndUpdate(req.post._id, { assignedTo }, { new: true })
     .populate("postUserId assignedTo")
-    .then((updatedPost) => {
-      console.log(updatedPost);
-      res.render("intranet/assign", { post: updatedPost });
+    .then((assignedPost) => {
+      // console.log(assignedPost);
+      const formatDate = formatedDate(req.post.created);
+      res.render("intranet/assign", {
+        post: assignedPost,
+        created: formatDate,
+      });
     });
 });
 
 //SEND ASSIGNED POST TO THE COMPANY USER
-router.post(
-  "/:id/assign/send",
-  isLoggedIn,
-  isAdmin,
-  PostMiddleware,
-  (req, res) => {
-    const { category, postText, companyName, name, created, assignedTo } =
-      req.body;
-    console.log(req.body);
+router.post("/:id/assign/send", isAdmin, PostMiddleware, (req, res) => {
+  const { category, postText, companyName, name, created, assignedTo } =
+    req.body;
+  // console.log(req.body);
+  console.log(req.post);
+
+  // User.findById(req.post.assignedTo._id).then((assignedUser) => {
+  //   //Check if the assignedTo is a company person
+  if (!req.post.assignedTo.isCompany) {
+    return res.redirect("/intranet/admin-dashboard");
   }
-);
+  //Check if the assignedTo has an email
+  if (!req.post.assignedTo.email) {
+    return res.status(400).render(`intranet/${req.params.id}/assign`, {
+      errorMessage: "The assigned person doesn't have an email.",
+    });
+  }
+
+  //Format Date to DD/MM/YYYY
+  const formatDate = formatedDate(req.post.created);
+
+  //Nodemailer execution
+  // 1. Create a Nodemailer transporter using either SMTP or some other transport mechanism
+  const transport = nodemailer.createTransport({
+    //I put the emailtrap parameters than I found on my account
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: process.env.EMAIL_TEST,
+      pass: process.env.PW_TEST,
+    },
+  });
+
+  const message = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+      <style>
+        body {
+          font-family: sans-serif;
+        }
+  
+        p {
+          margin: 2rem auto 0;
+          text-align: center;
+          max-width: 40ch;
+        }
+  
+        h1,
+        footer {
+          text-align: center;
+        }
+  
+        footer {
+          margin-top: 2rem;
+          background: #000;
+          color: white;
+          padding: 1.5rem;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>AMINE YOU HAVE A NEW ASSIGNED POST !</h1>
+  
+      <form>
+          <fieldset>
+              <legend class="h2">Details of the Post</legend>
+              <label>Category</label>
+              <input
+                type="text"
+                name="category"
+                value= ${req.post.category}
+                readonly
+              />
+              <br/>
+  
+              <label>Question</label>
+              <textarea
+                rows="5"
+                cols="30"
+                name="postText"
+                readonly
+              >${req.post.postText}</textarea>
+              <br/>
+  
+              <label>Company Name</label>
+              <input
+                type="text"
+                name="companyName"
+                value=${req.post.postUserId.companyName}
+                readonly
+              />
+              <br/>
+  
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                value=${req.post.postUserId.userName}
+                readonly
+              />
+              <br/>
+  
+              <label>Email</label>
+              <input
+                type="text"
+                name="email"
+                value=${req.post.postUserId.email}
+                readonly
+              />
+              <br/>
+  
+              <label>Phone Number</label>
+              <input
+                type="number"
+                name="phoneNumber"
+                value=${req.post.postUserId.phoneNumber}
+                readonly
+              />
+              <br/>
+  
+              <label>Location</label>
+              <input
+                type="text"
+                name="location"
+                value=${req.post.postUserId.location}
+                readonly
+              />
+              <br/>
+  
+              <label>Business</label>
+              <input
+                type="text"
+                name="business"
+                value=${req.post.postUserId.business}
+                readonly
+              />
+              <br/>
+  
+              <label>Job Title</label>
+              <input
+                type="text"
+                name="jobTitle"
+                value=${req.post.postUserId.jobTitle}
+                readonly
+              />
+              <br/>
+  
+              <label>Date</label>
+              <input 
+              type="text" 
+              name="created" 
+              value=${formatDate} 
+              readonly
+              />
+              <br/>
+  
+              <label>Assigned to</label>
+              <input
+                type="text"
+                name="assignedTo"
+                value=${req.post.assignedTo.userName}
+                readonly
+              />
+              <br/>
+              <br/>
+      </form>
+      
+      <footer>Nodemailer | 2021</footer>
+    </body>
+  </html>
+  `;
+
+  transport.sendMail({
+    from: '"Fred Foo 👻" <sendersaddress@here.com>', // sender address
+    to: "receiversaddress@here.com", // list of receivers
+    subject: "WE ARE ALIVE! 🔥", // Subject line
+    text: "Hello world?", // plain text body
+    html: message, // html body
+  });
+
+  // });
+  res.redirect("/intranet/admin-dashboard");
+});
 
 module.exports = router;
